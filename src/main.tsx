9c4bd625748c45faaf3052f67ac19e20cd29fd0f
ofadiman <ofadiman@gmail.com>
2024-01-27 20:20:49 +0100

    feature: show map as json based on search parameters

--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,22 @@ import { StrictMode } from 'react'
 import { useApi } from './api/useApi.ts'
 import { NodeFactory } from './components/NodeFactory.tsx'
 
-// const urlSearchParams = new URLSearchParams(window.location.search)
-// const device = urlSearchParams.get('layout') ?? 'desktop'
-// const shouldRenderMap = urlSearchParams.get('map') === 'true'
-
 const App = () => {
+  const urlSearchParams = new URLSearchParams(window.location.search)
+  const shouldRenderMap = urlSearchParams.get('map') === 'true'
   const root = useApi()
 
   if (root === null) {
     return <p>Loading...</p>
   }
 
+  if (shouldRenderMap === true) {
+    return <pre>{JSON.stringify(root, null, 2)}</pre>
+  }
+
   return <NodeFactory node={root} />
 }
 
-// TODO: Handle rendering website map as JSON.
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
